d5d88468e7947ee0e5f37e742ee23d0faf8e4703
Kyle <kyle.c.r.fahey@gmail.com>
2016-10-18 19:53:14 -0500

    Add Markers Add Places API Call

--- js/magic.js
+++ js/magic.js
@@ -15,26 +15,21 @@ var geo = {
   })()
 };
 
+var map;
+var info;
+var service;
+
 function initMap() {
   var userLocation = {
   lat: geo.local.latitude,
   lng: geo.local.longitude
 };
-var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
     zoom:17,
     center: userLocation
   });
-var marker = new google.maps.Marker({
-  position: userLocation,
-  map: map
-});
-var infowindow = new google.maps.InfoWindow({
-  content: "hello world"
-});
-marker.addListener('click', function() {
-  infowindow.open(map, marker);
-});
-
+ infoWindow = new google.maps.InfoWindow();
+ service = new google.maps.places.PlacesService(map);
 };
 
 console.log(geo.local.longitude);
@@ -43,3 +38,47 @@ console.log(geo.local.latitude);
 setTimeout(function() {
   initMap();
 }, 5000);
+
+function performSearch() {
+    var request = {
+        keyword: 'wal' //KO Clicakble
+    };
+    service.radarSearch(request, callback);
+}
+
+function callback(results , status) {
+    if ( status !== google.maps.places.PlacesServiceStatus.OK) {
+        console.error(status);
+        return;
+    }
+    for (var i = 0, result; result = results[i]; i++){
+        addMarker(result);
+    }
+
+}
+
+function addMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        postition: place.geometry.location,
+        icon: {
+            url: 'http://maps.gstatic.com/mapfiles/circle.png',
+            anchor: new google.maps.Point(10,10),
+            scaledSize: new google.maps.Size(10,17)
+        }
+
+        });
+
+    google.maps.event.addEventListener(marker, 'click', function() {
+        service.getDetails(place, function(result, status) {
+            if (status !== google.maps.place.PlacesServiceStatus.OK) {
+                console.error(status);
+                return;
+            }
+            infoWindow.setContent(result.name);
+            infoWindow.open(map, marker);
+        });
+    });
+}
+performSearch();
+addMarker();
