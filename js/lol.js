472c79465ccdc7d8dd481d6e55b630dfc672e20b
Kyle <kyle.c.r.fahey@gmail.com>
2016-10-19 08:44:03 -0500

    Bug Testing

---
+++ js/lol.js
@@ -0,0 +1,84 @@
+var geo = {
+  local: {
+    longitude: "",
+    latitude: "",
+    positionFound: false
+  },
+  location: (function(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        geo.local.longitude = position.coords.longitude;
+        geo.local.latitude = position.coords.latitude;
+        geo.local.positionFound = true;
+      });
+    }
+  })()
+};
+
+var map;
+var info;
+var service;
+
+function initMap() {
+  var userLocation = {
+  lat: geo.local.latitude,
+  lng: geo.local.longitude
+};
+    map = new google.maps.Map(document.getElementById('map'), {
+    zoom:17,
+    center: userLocation
+  });
+ infoWindow = new google.maps.InfoWindow();
+ service = new google.maps.places.PlacesService(map);
+};
+
+console.log(geo.local.longitude);
+console.log(geo.local.latitude);
+
+setTimeout(function() {
+  initMap();
+}, 5000);
+
+function performSearch() {
+    var request = {
+        keyword: 'wal' //KO Clicakble
+    };
+    service.radarSearch(request, callback);
+}
+
+function callback(results , status) {
+    if ( status !== google.maps.places.PlacesServiceStatus.OK) {
+        console.error(status);
+        return;
+    }
+    for (var i = 0, result; result = results[i]; i++){
+        addMarker(result);
+    }
+
+}
+
+function addMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        postition: place.geometry.location,
+        icon: {
+            url: 'http://maps.gstatic.com/mapfiles/circle.png',
+            anchor: new google.maps.Point(10,10),
+            scaledSize: new google.maps.Size(10,17)
+        }
+
+        });
+
+    google.maps.event.addEventListener(marker, 'click', function() {
+        service.getDetails(place, function(result, status) {
+            if (status !== google.maps.place.PlacesServiceStatus.OK) {
+                console.error(status);
+                return;
+            }
+            infoWindow.setContent(result.name);
+            infoWindow.open(map, marker);
+        });
+    });
+}
+performSearch();
+addMarker();
--- js/magic.js
+++ js/magic.js
@@ -16,69 +16,74 @@ var geo = {
 };
 
 var map;
-var info;
+var infoWindow;
 var service;
 
 function initMap() {
   var userLocation = {
   lat: geo.local.latitude,
   lng: geo.local.longitude
-};
-    map = new google.maps.Map(document.getElementById('map'), {
-    zoom:17,
-    center: userLocation
+}
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: userLocation,
+    zoom: 15,
+    styles: [{
+      stylers: [{ visibility: 'simplified' }]
+    }, {
+      elementType: 'labels',
+      stylers: [{ visibility: 'off' }]
+    }]
   });
- infoWindow = new google.maps.InfoWindow();
- service = new google.maps.places.PlacesService(map);
-};
 
-console.log(geo.local.longitude);
-console.log(geo.local.latitude);
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+
+  // The idle event is a debounced event, so we can query & listen without
+  // throwing too many requests at the server.
+  map.addListener('idle', performSearch);
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'tires'
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    console.error(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    addMarker(result);
+  }
+}
+
+function addMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: {
+      url: 'http://maps.gstatic.com/mapfiles/circle.png',
+      anchor: new google.maps.Point(10, 10),
+      scaledSize: new google.maps.Size(10, 17)
+    }
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        console.error(status);
+        return;
+      }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
+    });
+  });
+}
 
 setTimeout(function() {
   initMap();
 }, 5000);
-
-function performSearch() {
-    var request = {
-        keyword: 'wal' //KO Clicakble
-    };
-    service.radarSearch(request, callback);
-}
-
-function callback(results , status) {
-    if ( status !== google.maps.places.PlacesServiceStatus.OK) {
-        console.error(status);
-        return;
-    }
-    for (var i = 0, result; result = results[i]; i++){
-        addMarker(result);
-    }
-
-}
-
-function addMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        postition: place.geometry.location,
-        icon: {
-            url: 'http://maps.gstatic.com/mapfiles/circle.png',
-            anchor: new google.maps.Point(10,10),
-            scaledSize: new google.maps.Size(10,17)
-        }
-
-        });
-
-    google.maps.event.addEventListener(marker, 'click', function() {
-        service.getDetails(place, function(result, status) {
-            if (status !== google.maps.place.PlacesServiceStatus.OK) {
-                console.error(status);
-                return;
-            }
-            infoWindow.setContent(result.name);
-            infoWindow.open(map, marker);
-        });
-    });
-}
-performSearch();
-addMarker();
