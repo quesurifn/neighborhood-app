eb43c2a9b8224c0f402c97d74db4317c0274ad82
Kyle <kyle.c.r.fahey@gmail.com>
2016-12-23 12:02:50 -0600

    add coffee as default option

--- master/js/jquery.js
+++ master/js/jquery.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $("#menu").on('click', function() {
       $("#mobile-list").toggle();
     });
-});
+    setTimeout(function() {
+      $('#default').trigger('click');
+    }, 1000);
+  });
